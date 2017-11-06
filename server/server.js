10b5d8011de9b2abfdaa52cf48af630b9ed74c43
Xong Xiong <xxiong3153@gmail.com>
2017-11-06 15:32:49 -0600

    added mlab config

--- server/server.js
+++ server/server.js
@@ -15,9 +15,16 @@ app.use('/rentals', rentals);
 
 /** ---------- MONGOOSE ---------------- */
 var mongoose = require('mongoose');
-// gamestop is the name of our database
-// 27017 is the port number
-var databaseUrl = 'mongodb://localhost:27017/realestate';
+var mongoURI = '';
+// process.env.MONGODB_URI will only be defined if you
+// are running on Heroku
+if (process.env.MONGODB_URI) {
+    // use the string value of the environment variable
+    mongoURI = process.env.MONGODB_URI;
+} else {
+    // use the local database server
+    mongoURI = 'mongodb://localhost:27017/realestate';
+}
 
 mongoose.connection.on('connected', function () {
     console.log('mongoose is connected!');
@@ -27,7 +34,7 @@ mongoose.connection.on('error', function () {
     console.log('mongoose connection failed');
 });
 
-mongoose.connect(databaseUrl);
+mongoose.connect(mongoURI);
 // Eventually, the mongooose code should be in a module
 
 /** ---------- START SERVER ---------- **/
